bb47ce0e54a0fd9afb4c1dc1b2525b5f7d8a3177
camur <camur@indra.es>
2023-03-27 12:07:10 +0200

    add: Fisrt commit

---
+++ cordova/README.md
@@ -0,0 +1,7 @@
+# Cordova-Hybrid-Base
+Carcasa base para aplicaciones hibridas con cordova
+
+# Instrucciones
+- npm install -g cordova
+- npm run cordova:setup
+- npm run android:emulate
---
+++ cordova/www/static/19mcDkm20EU8B6DhTrZOG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/50901216b76e581e.css","static/chunks/pages/index-15d22f8a98f1b07c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
